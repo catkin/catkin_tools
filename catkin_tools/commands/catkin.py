d5c993c0edafd62686836e2f8e3c94f583f3c67a
William Woodall <william@osrfoundation.org>
2014-03-31 11:43:00 -0700

    give more visibility to the prevention of infinite recursion for verb alias expansion

--- catkin_tools/commands/catkin.py
+++ catkin_tools/commands/catkin.py
@@ -108,19 +108,35 @@ def main(sysargs=None):
 
     # Do alias expansion
     expanding_verb_aliases = True
+    used_aliases = []
     while expanding_verb_aliases:
         expanding_verb_aliases = False
         for index, arg in enumerate(sysargs):
             if not arg.startswith('-'):
+                if arg in used_aliases:
+                    print(fmt(
+                        "@!@{yf}Warning:@| The verb alias '@!@{yf}" +
+                        arg +
+                        "@|' was previously expanded, ignoring this time to prevent infinite recursion."
+                    ))
                 if arg in verb_aliases:
-                    old_sysargs = list(sysargs)
                     before = [] if index == 0 else sysargs[:index - 1]
                     after = [] if index == len(sysargs) else sysargs[index + 1:]
                     sysargs = before + verb_aliases[arg].split() + after
-                    print(fmt("@!@{gf}==>@| Expanding alias '@!@{yf}{0}@|' from '@!@{yf}{1}@|' to '@!@{yf}{2}@|'")
-                          .format(arg, ' '.join([cmd] + old_sysargs), ' '.join([cmd] + sysargs)))
+                    print(fmt(
+                        "@!@{gf}==>@| Expanding alias "
+                        "'@!@{yf}{alias}@|' "
+                        "from '@{yf}{before} @!{alias}@{boldoff} {after}@|' "
+                        "to '@{yf}{before} @!{expansion}@{boldoff} {after}@|'"
+                    ).format(
+                        alias=arg,
+                        expansion=verb_aliases[arg],
+                        before=' '.join([cmd] + before),
+                        after=' '.join(after)
+                    ))
                     expanding_verb_aliases = True
-                    # Remove the alias from verb_aliases to prevent infinite recursion
+                    # Prevent the alias from being used again, to prevent infinite recursion
+                    used_aliases.append(arg)
                     del verb_aliases[arg]
                 break
 
