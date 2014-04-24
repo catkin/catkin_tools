ebc3352806e3f7a009641e14b6853c450fbb7336
William Woodall <william@osrfoundation.org>
2014-04-23 23:40:56 -0700

    refactor which() into top level utils module

--- catkin_tools/verbs/catkin_build/common.py
+++ catkin_tools/verbs/catkin_build/common.py
@@ -611,31 +611,3 @@ def wide_log(msg, **kwargs):
     """
     global wide_log_fn
     wide_log_fn(msg, **kwargs)
-
-
-def which(program):
-    """Custom version of the ``which`` built-in shell command.
-
-    Searches the pathes in the ``PATH`` environment variable for a given
-    executable name. It returns the full path to the first instance of the
-    executable found or None if it was not found.
-
-    :param program: name of the executable to find
-    :type program: str
-    :returns: Full path to the first instance of the executable, or None
-    :rtype: str or None
-    """
-    def is_exe(fpath):
-        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, _ = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ.get('PATH', os.defpath).split(os.pathsep):
-            path = path.strip('"')
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-    return None
