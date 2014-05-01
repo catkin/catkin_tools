e27643deba97815e4861f58ef9babc0e82ea62a9
William Woodall <william@osrfoundation.org>
2014-04-30 18:22:16 -0700

    fix installation of notification resources

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from setuptools import setup
@@ -13,10 +14,30 @@ install_requires = [
 if sys.version_info[0] == 2 and sys.version_info[1] <= 6:
     install_requires.append('argparse')
 
+this_dir = os.path.abspath(os.path.dirname(__file__))
+osx_resources_path = os.path.join(
+    this_dir,
+    'catkin_tools',
+    'notifications',
+    'resources',
+    'osx',
+    'catkin build.app')
+osx_notification_resources = [os.path.join(dp, f)
+                              for dp, dn, fn in os.walk(osx_resources_path)
+                              for f in fn]
+src_path = os.path.join(this_dir, 'catkin_tools')
+osx_notification_resources = [os.path.relpath(x, src_path)
+                              for x in osx_notification_resources]
+
 setup(
     name='catkin_tools',
     version='0.0.0',
     packages=find_packages(exclude=['tests', 'docs']),
+    package_data={
+        'catkin_tools': [
+            'notifications/resources/linux/catkin_icon.png',
+        ] + osx_notification_resources
+    },
     install_requires=install_requires,
     author='William Woodall',
     author_email='william@osrfoundation.org',
