f17b1b28abfb33be6b5ec42b439031bc9bfbf417
Jonathan Bohren <jbo@jhu.edu>
2014-06-11 00:12:30 -0400

    build: Added --extend argument to control env
    This enables the building of a workspace against an arbitrary realized
    catkin devel- or install-space which might not currently be sourced.
    This is equivalent to sourcing the setup files in the given directory in
    a subshell, building, and then exiting.
    
    - The argument to `--extend` should be a path containing valid catkin
      setup files
    - It prints out CMAKE_PREFIX_PATH at the top of the build command
    - Calling `--extend` also implicitly enables `--force-cmake`
    - Calling `--extend ""` (with an empty string) is invalid and will be
      reported in an error like specifying a path which doesn't exist.

--- catkin_tools/verbs/catkin_build/build.py
+++ catkin_tools/verbs/catkin_build/build.py
@@ -18,6 +18,7 @@ import os
 import stat
 import sys
 import time
+import re
 
 from multiprocessing import cpu_count
 from threading import Lock
@@ -54,6 +55,7 @@ from .common import get_recursive_run_depends_in_workspace
 from .common import is_tty
 from .common import log
 from .common import wide_log
+from .common import run_command
 
 from .executor import Executor
 from .executor import ExecutorEvent
@@ -62,6 +64,88 @@ from .job import CatkinJob
 from .job import CMakeJob
 
 from .output import OutputController
+
+
+def get_resultspace_environment(result_space_path, quiet=False):
+    """Get the environemt variables which result from sourcing another catkin
+    workspace's setup files as the string output of `cmake -E environment`.
+    This command is used to be as portable as possible.
+
+    :param result_space_path: path to a Catkin result-space whose environment should be loaded, ``str``
+    :param quiet: don't throw exceptions, ``bool``
+
+    :returns: a carriage-return-delimted string of environment variables, ``str``
+    """
+    # Check to make sure result_space_path is a valid directory
+    if not os.path.isdir(result_space_path):
+        if quiet:
+            return dict()
+        raise IOError(
+            "Cannot load environment from resultspace \"%s\" because it does not "
+            "exist." % result_space_path
+        )
+
+    # Check to make sure result_space_path contains a `.catkin` file
+    # TODO: `.catkin` should be defined somewhere as an atom in catkin_pkg
+    if not os.path.exists(os.path.join(result_space_path, '.catkin')):
+        if quiet:
+            return dict()
+        raise IOError(
+            "Cannot load environment from resultspace \"%s\" because it does not "
+            "appear to be a catkin-generated resultspace (missing .catkin marker "
+            "file)." % result_space_path
+        )
+
+    # Determine the shell to use to source the setup file
+    shell_path = os.environ['SHELL']
+    (_, shell_name) = os.path.split(shell_path)
+
+    # Use fallback shell if using a non-standard shell
+    if shell_name not in ['bash', 'zsh']:
+        shell_name = 'bash'
+
+    # Check to make sure result_space_path contains the appropriate setup file
+    setup_file_path = os.path.join(result_space_path, 'setup.%s' % shell_name)
+    if not os.path.exists(setup_file_path):
+        if quiet:
+            return dict()
+        raise IOError(
+            "Cannot load environment from resultspace \"%s\" because the "
+            "required setup file \"%s\" does not exist." % (result_space_path, setup_file_path)
+        )
+
+    # Construct a command list which sources the setup file and prints the env to stdout
+    norc_flags = {'bash': '--norc', 'zsh': '-f'}
+    subcommand = 'source %s; cmake -E environment' % (setup_file_path)
+
+    command = [
+        shell_path,
+        norc_flags[shell_name],
+        '-c', subcommand]
+
+    # Run the command to source the other environment and output all environment variables
+    blacklisted_keys = ('_', 'PWD')
+    env_regex = re.compile('(.+?)=(.*)$', re.M)
+    env_dict = dict()
+
+    for line in run_command(command, cwd=os.getcwd()):
+        if isinstance(line, str):
+            matches = env_regex.findall(line)
+            for (key, value) in matches:
+                if key not in blacklisted_keys:
+                    env_dict[key] = value.rstrip()
+
+    return env_dict
+
+
+def load_resultspace_environment(result_space_path):
+    """Load the environemt variables which result from sourcing another
+    workspace path into this process's environment.
+    
+    :param result_space_path: path to a Catkin result-space whose environment should be loaded, ``str``
+    """
+    env_dict = get_resultspace_environment(result_space_path)
+    os.environ.update(env_dict)
 
 
 def get_ready_packages(packages, running_jobs, completed):
