d0b67f713403148b243890a45a8e2b3575066dc7
William Woodall <william@osrfoundation.org>
2014-10-24 14:51:30 -0700

    [style] remove unused code and fix whitespace

--- tests/integrated/resources/pkg_with_test/test_good.py
+++ tests/integrated/resources/pkg_with_test/test_good.py
@@ -2,9 +2,10 @@
 
 import unittest
 
+
 class TestGood(unittest.TestCase):
     def test_zero(self):
-        self.assertEqual(0,0)
+        self.assertEqual(0, 0)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/workspace_assertions.py
+++ tests/workspace_assertions.py
@@ -1,23 +1,14 @@
 
 from __future__ import print_function
 
-import functools
-import os
 import re
-import shutil
-import sys
-import tempfile
 
-import subprocess
-import unittest
-
-from .utils import assert_raises_regex
-from .utils import in_temporary_directory
-from .utils import redirected_stdio
 from .utils import assert_files_exist
+
 
 def assert_workspace_initialized(path):
     assert_files_exist(path, ['.catkin_tools'])
+
 
 def assert_warning_message(out_str, pattern=''):
     """
@@ -31,6 +22,7 @@ def assert_warning_message(out_str, pattern=''):
     if pattern:
         found = re.findall(pattern, out_str_stripped)
         assert len(found) > 0
+
 
 def assert_no_warnings(out_str):
     """
