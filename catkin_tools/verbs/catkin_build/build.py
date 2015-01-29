66202f4e4eaa0b3b75098d7610a7de9352556e8b
Kei Okada <kei.okada@gmail.com>
2015-01-29 11:42:15 +0900

    fix to match pep8

--- catkin_tools/verbs/catkin_build/build.py
+++ catkin_tools/verbs/catkin_build/build.py
@@ -464,7 +464,7 @@ def build_isolated_workspace(
         e.start()
 
     # status_update_rate is positive, and non zero, also 10Hz is the fastest refresh rate
-    if not status_update_rate is False and (status_update_rate > 10.0 or 0 > status_update_rate):
+    if status_update_rate is not False and (status_update_rate > 10.0 or 0 > status_update_rate):
         log("Failed to set status_update_rate '{}', it must to be beteen 0.0 to 10.0, Force set to 10".format(status_update_rate))
         status_update_rate = 10
 
