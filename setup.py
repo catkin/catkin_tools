fc660e6cfc85e579a68f4b0d23ccaaeb0ec394a7
William Woodall <william@osrfoundation.org>
2015-06-02 15:42:14 -0700

    ordering imports

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
+from distutils import log
 import os
 import platform
-import sys
-
+from setuptools import find_packages
 from setuptools import setup
 from setuptools.command.install import install
-from setuptools import find_packages
-from distutils import log
 from stat import ST_MODE
+import sys
 
 
 # Setup installation dependencies
