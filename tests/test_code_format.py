00531f6ee44b1af0b6ceb32efcb96af2e9e30e5e
William Woodall <william@osrfoundation.org>
2015-11-10 11:36:49 -0800

    fix code style test to work with newer versions of flake8

--- tests/test_code_format.py
+++ tests/test_code_format.py
@@ -8,7 +8,11 @@ def test_flake8():
     report = flake8style.options.report
     report.start()
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    flake8style.input_dir(os.path.join(this_dir, '..', 'catkin_tools'))
+    try:
+        input_dir = flake8style.input_dir
+    except AttributeError:
+        input_dir = flake8style._styleguide.input_dir
+    input_dir(os.path.join(this_dir, '..', 'catkin_tools'))
     report.stop()
     assert report.total_errors == 0, \
         ("Found '{0}' code style errors (and warnings)."
