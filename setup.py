25d1b10052799616a178a6579f4595bd56431fd2
William Woodall <william@osrfoundation.org>
2016-04-18 16:06:17 -0700

    change version dependency on osrf_pycommon

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = [
     'catkin-pkg > 0.2.9',
     'setuptools',
     'PyYAML',
-    'osrf-pycommon==0.1.2',
+    'osrf-pycommon > 0.1.1',
 ]
 if sys.version_info[0] == 2 and sys.version_info[1] <= 6:
     install_requires.append('argparse')
