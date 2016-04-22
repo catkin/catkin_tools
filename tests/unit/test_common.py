63e554c99df8aa91f62427b7330f365eaa23b4f0
William Woodall <william@osrfoundation.org>
2016-04-22 15:49:16 -0700

    test for #356

--- tests/unit/test_common.py
+++ tests/unit/test_common.py
@@ -31,3 +31,19 @@ def test_get_recursive_build_depends_in_workspace_with_test_depend():
 
     r = common.get_recursive_build_depends_in_workspace(pkg1, ordered_packages)
     assert r == ordered_packages[1:], r
+
+
+def test_format_time_delta_short():
+    inputs = {
+        1.45: "1.4",
+        61.45: "01:01.4",
+        121.45: "02:01.4",
+        # this one tests: https://github.com/catkin/catkin_tools/issues/356
+        3601.45: "1:00:01.4",
+        3721.45: "1:02:01.4",
+        7321.45: "2:02:01.4",
+        93821.45: "1 day 2:03:41.5",
+    }
+    for k, v in inputs.items():
+        f = common.format_time_delta_short(k)
+        assert f == v, "format_time_delta_short({0}) -> '{1}' != '{2}'".format(k, f, v)
