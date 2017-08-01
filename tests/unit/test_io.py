7ab463d0473d47f4639ccea5f92cbb1960386999
JD Yamokoski <jyamokoski@houstonmechatronics.com>
2017-08-01 11:02:55 -0500

    Fixes unknown 'to_bytes' unittest for Python2

--- tests/unit/test_io.py
+++ tests/unit/test_io.py
@@ -5,12 +5,13 @@ except ImportError:
     # Python2
     from Queue import Queue
 
+import binascii
 from catkin_tools.execution import io as io
 
 
 def test_invalid_utf8_characters():
     io_container = io.IOBufferContainer('test', 'test_id', 'test', Queue(), '/tmp')
-    bad_byte_array = (128).to_bytes(1, byteorder='big')
+    bad_byte_array = binascii.a2b_hex(b'80')
     try:
         io_container._decode(bad_byte_array)
     except UnicodeDecodeError:
