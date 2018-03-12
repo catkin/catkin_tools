1623d250207e672f2ec8ec55555c1bfec556162a
Mike Purvis <mpurvis@clearpathrobotics.com>
2018-03-11 22:57:31 -0400

    Fix OSX python story.

--- .travis.before_install.bash
+++ .travis.before_install.bash
@@ -4,10 +4,14 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   sudo apt-get install enchant -y
 elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
   if [ "$PYTHON" == "/usr/local/bin/python3" ]; then
-    brew install python3
+    # Brewed Python 3.
+    brew upgrade python
+  elif [ "$PYTHON" == "/usr/local/bin/python2" ]; then
+    # Brewed Python 2.
+    brew install python@2
   fi
+  $PYTHON -m pip install virtualenv
+  $PYTHON -m virtualenv -p $PYTHON venv
   brew install enchant
-  sudo pip install virtualenv
-  virtualenv -p $PYTHON venv
   source venv/bin/activate
 fi
