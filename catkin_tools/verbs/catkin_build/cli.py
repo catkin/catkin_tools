4ab4e25d598ecdfe8ae8cfd9831a3b75576687e8
Johannes Meyer <johannes@intermodalics.eu>
2018-03-12 16:27:23 +0100

    Respect VERBOSE environment variable if already set before in verbose mode (#506)

--- catkin_tools/verbs/catkin_build/cli.py
+++ catkin_tools/verbs/catkin_build/cli.py
@@ -398,7 +398,7 @@ def main(opts):
         parallel_jobs = None
 
     # Set VERBOSE environment variable
-    if opts.verbose:
+    if opts.verbose and 'VERBOSE' not in os.environ:
         os.environ['VERBOSE'] = '1'
 
     try:
