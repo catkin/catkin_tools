404ed5f465a48dd08393c9a3b85ad2018c6eba25
Timon Engelke <timonegk@users.noreply.github.com>
2021-07-18 13:24:49 +0200

    Switch from Travis CI to GitHub actions (#684)
    * Add GitHub actions configuration
    
    * Remove travis configuration
    
    * Update CI badge in README

--- .travis.before_script.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  sudo apt-get install cmake build-essential
-elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  echo "No OS X-specific before_script steps."
-fi
