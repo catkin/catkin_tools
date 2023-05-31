860a0b8a66e23cccc04c782f385125b11ad04b71
Jonas Zohren <15788906+jfowl@users.noreply.github.com>
2023-05-31 15:59:40 +0200

    fix: typo

--- docs/examples/ros_tutorials_ws/0_checkout.bash
+++ docs/examples/ros_tutorials_ws/0_checkout.bash
@@ -1,7 +1,7 @@
 export ROS_DISTRO=noetic                                 # Set ROS distribution
 mkdir -p /tmp/ros_tutorials_ws/src                       # Create workspace
 cd /tmp/ros_tutorials_ws/src                             # Navigate to source space
-rosinstall_generator --deps ros_tutorials > .rosinstall  # Get list of pakcages
+rosinstall_generator --deps ros_tutorials > .rosinstall  # Get list of packages
 wstool update                                            # Checkout all packages
 cd /tmp/ros_tutorials_ws                                 # Navigate to ros workspace root
 catkin init                                              # Initialize workspace
