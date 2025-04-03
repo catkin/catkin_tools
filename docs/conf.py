8ef11ff40514ea9cdb973e4f8486fcc26f5eadcf
Timon Engelke <timon.engelke@online.de>
2025-04-03 21:31:06 +0200

    ci: modernize github workflow

--- docs/conf.py
+++ docs/conf.py
@@ -275,7 +275,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {'python': ('http://docs.python.org/', None)}
 
 # programoutput options
 #programoutput_use_ansi = True
